7ae5b06fef6fc9ccf0924941f53713f1dd603cd8
Chamika Sandun <110515504+Dodamwaththacs@users.noreply.github.com>
2024-04-02 17:37:41 +0530

    add file Endpoints

---
+++ src/main/java/io/bootify/health_hive/model/FileDTO.java
@@ -0,0 +1,73 @@
+package io.bootify.health_hive.model;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.time.LocalDate;
+
+
+public class FileDTO {
+
+    private Long id;
+
+    @NotNull
+    @Size(max = 255)
+    private String name;
+
+    @NotNull
+    private FileType type;
+
+    private LocalDate createdDate;
+
+    private Long labDataUpload;
+
+    private Long dataUploadReqeust;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public FileType getType() {
+        return type;
+    }
+
+    public void setType(final FileType type) {
+        this.type = type;
+    }
+
+    public LocalDate getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(final LocalDate createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Long getLabDataUpload() {
+        return labDataUpload;
+    }
+
+    public void setLabDataUpload(final Long labDataUpload) {
+        this.labDataUpload = labDataUpload;
+    }
+
+    public Long getDataUploadRequest() {
+        return dataUploadReqeust;
+    }
+
+    public void setDataUploadReqeust(final Long dataUploadReqeust) {
+        this.dataUploadReqeust = dataUploadReqeust;
+    }
+
+}
---
+++ src/main/java/io/bootify/health_hive/service/FileService.java
@@ -0,0 +1,84 @@
+package io.bootify.health_hive.service;
+
+import io.bootify.health_hive.domain.DataUploadRequest;
+import io.bootify.health_hive.domain.File;
+import io.bootify.health_hive.domain.LabDataUpload;
+import io.bootify.health_hive.model.FileDTO;
+import io.bootify.health_hive.repos.DataUploadRequestRepository;
+import io.bootify.health_hive.repos.FileRepository;
+import io.bootify.health_hive.repos.LabDataUploadRepository;
+import io.bootify.health_hive.util.NotFoundException;
+import java.util.List;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class FileService {
+
+    private final FileRepository fileRepository;
+    private final LabDataUploadRepository labDataUploadRepository;
+    private final DataUploadRequestRepository dataUploadRequestRepository;
+
+    public FileService(final FileRepository fileRepository,
+                       final LabDataUploadRepository labDataUploadRepository,
+                       final DataUploadRequestRepository dataUploadRequestRepository) {
+        this.fileRepository = fileRepository;
+        this.labDataUploadRepository = labDataUploadRepository;
+        this.dataUploadRequestRepository = dataUploadRequestRepository;
+    }
+
+    public List<FileDTO> findAll() {
+        final List<File> files = fileRepository.findAll(Sort.by("id"));
+        return files.stream()
+                .map(file -> mapToDTO(file, new FileDTO()))
+                .toList();
+    }
+
+    public FileDTO get(final Long id) {
+        return fileRepository.findById(id)
+                .map(file -> mapToDTO(file, new FileDTO()))
+                .orElseThrow(NotFoundException::new);
+    }
+
+    public Long create(final FileDTO fileDTO) {
+        final File file = new File();
+        mapToEntity(fileDTO, file);
+        return fileRepository.save(file).getId();
+    }
+
+    public void update(final Long id, final FileDTO fileDTO) {
+        final File file = fileRepository.findById(id)
+                .orElseThrow(NotFoundException::new);
+        mapToEntity(fileDTO, file);
+        fileRepository.save(file);
+    }
+
+    public void delete(final Long id) {
+        fileRepository.deleteById(id);
+    }
+
+    private FileDTO mapToDTO(final File file, final FileDTO fileDTO) {
+        fileDTO.setId(file.getId());
+        fileDTO.setName(file.getName());
+        fileDTO.setType(file.getType());
+        fileDTO.setCreatedDate(file.getCreatedDate());
+        fileDTO.setLabDataUpload(file.getLabDataUpload() == null ? null : file.getLabDataUpload().getId());
+        fileDTO.setDataUploadReqeust(file.getDataUploadRequest() == null ? null : file.getDataUploadRequest().getId());
+        return fileDTO;
+    }
+
+    private File mapToEntity(final FileDTO fileDTO, final File file) {
+        file.setName(fileDTO.getName());
+        file.setType(fileDTO.getType());
+        file.setCreatedDate(fileDTO.getCreatedDate());
+        final LabDataUpload labDataUpload = fileDTO.getLabDataUpload() == null ? null : labDataUploadRepository.findById(fileDTO.getLabDataUpload())
+                .orElseThrow(() -> new NotFoundException("labDataUpload not found"));
+        file.setLabDataUpload(labDataUpload);
+        final DataUploadRequest dataUploadReqeust = fileDTO.getDataUploadRequest() == null ? null : dataUploadRequestRepository.findById(fileDTO.getDataUploadRequest())
+                .orElseThrow(() -> new NotFoundException("dataUploadReqeust not found"));
+        file.setDataUploadRequest(dataUploadReqeust);
+        return file;
+    }
+
+}
