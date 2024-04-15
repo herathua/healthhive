5487db34e9be6f3ec91f727cf55d2f5a939328f5
herathua <110981456+herathua@users.noreply.github.com>
2024-04-16 01:57:08 +0530

    add message broker

--- src/main/java/io/bootify/health_hive/rest/LabRequestResource.java
+++ src/main/java/io/bootify/health_hive/rest/LabRequestResource.java
@@ -6,38 +6,31 @@ import io.bootify.health_hive.util.ReferencedException;
 import io.bootify.health_hive.util.ReferencedWarning;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
-import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api/labRequests", produces = MediaType.APPLICATION_JSON_VALUE)
 public class LabRequestResource {
 
     private final LabRequestService labRequestService;
+    private final SimpMessagingTemplate messagingTemplate;
 
-    public LabRequestResource(final LabRequestService labRequestService) {
+    @Autowired
+    public LabRequestResource(LabRequestService labRequestService, SimpMessagingTemplate messagingTemplate) {
         this.labRequestService = labRequestService;
-    }
-
-    @GetMapping
-    public ResponseEntity<List<LabRequestDTO>> getAllLabRequests() {
-        return ResponseEntity.ok(labRequestService.findAll());
+        this.messagingTemplate = messagingTemplate;
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<LabRequestDTO> getLabRequest(@PathVariable(name = "id") final Long id) {
-        return ResponseEntity.ok(labRequestService.get(id));
+        LabRequestDTO labRequestDTO = labRequestService.get(id);
+        //messagingTemplate.convertAndSend("/topic/lab/" + id, "Lab data updated"); // Sending WebSocket message
+        return ResponseEntity.ok(labRequestDTO);
     }
 
     @PostMapping
@@ -45,12 +38,13 @@ public class LabRequestResource {
     public ResponseEntity<Long> createLabRequest(
             @RequestBody @Valid final LabRequestDTO labRequestDTO) {
         final Long createdId = labRequestService.create(labRequestDTO);
+        messagingTemplate.convertAndSend("/topic/lab/" + labRequestDTO, "Lab data updated"); // Sending WebSocket message
         return new ResponseEntity<>(createdId, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Long> updateLabRequest(@PathVariable(name = "id") final Long id,
-            @RequestBody @Valid final LabRequestDTO labRequestDTO) {
+                                                 @RequestBody @Valid final LabRequestDTO labRequestDTO) {
         labRequestService.update(id, labRequestDTO);
         return ResponseEntity.ok(id);
     }
@@ -65,5 +59,4 @@ public class LabRequestResource {
         labRequestService.delete(id);
         return ResponseEntity.noContent().build();
     }
-
 }
