5d9ae621cc278649b29443eab6f2d7783eaf9309
herathua <110981456+herathua@users.noreply.github.com>
2024-04-15 21:23:21 +0530

    FileResource is commented all methods

--- src/main/java/io/bootify/health_hive/rest/FileResource.java
+++ src/main/java/io/bootify/health_hive/rest/FileResource.java
@@ -1,4 +1,5 @@
 package io.bootify.health_hive.rest;
+
 import io.bootify.health_hive.model.FileDTO;
 import io.bootify.health_hive.model.LabDataUploadDTO;
 import io.bootify.health_hive.service.FileService;
@@ -9,7 +10,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -18,26 +18,25 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-
 @RestController
 @RequestMapping(value = "/api/files", produces = MediaType.APPLICATION_JSON_VALUE)
 public class FileResource {
-
     private final FileService fileService;
     private final LabDataUploadService labDataUploadService;
-
-
     public FileResource(final FileService fileService, LabDataUploadService labDataUploadService) {
         this.fileService = fileService;
         this.labDataUploadService = labDataUploadService;
     }
-
+    /**
+     * Retrieve all files.
+     */
     @GetMapping
     public ResponseEntity<List<FileDTO>> getAllFiles() {
         return ResponseEntity.ok(fileService.findAll());
     }
-
+    /**
+     * Retrieve a file by ID and return it as a byte array.
+     */
     @GetMapping("/{id}")
     public ResponseEntity<ByteArrayResource> getFile(@PathVariable(name = "id") final Long id) {
         FileDTO fileDTO = fileService.get(id);
@@ -45,54 +44,58 @@ public class FileResource {
                 .contentType(MediaType.parseMediaType(fileDTO.getType()))
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileDTO.getName() + "\"")
                 .body(fileService.getFile(fileDTO.getFilePath()));
-
     }
-
+    /**
+     * Create a new file.
+     */
     @PostMapping
     @ApiResponse(responseCode = "201")
     public ResponseEntity<Long> createFile(
             @RequestParam(name = "labRequestId") final Long labRequestId,
             @RequestPart("file") MultipartFile file) throws IOException {
+        // Create file DTO and lab data upload DTO
         FileDTO fileDTO = new FileDTO();
         LabDataUploadDTO labDataUploadDTO = new LabDataUploadDTO();
-        String FileType = file.getContentType();
+        // Extract file details
+        String fileType = file.getContentType();
         String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        // Create lab data upload entity
         final Long createdFileId = labDataUploadService.create(labDataUploadDTO);
-        String FilePath = fileService.saveFile(file);
+        // Save file to filesystem
+        String filePath = fileService.saveFile(file);
+        // Populate DTOs with data
         labDataUploadDTO.setDescription(fileName);
         labDataUploadDTO.setLabRequest(labRequestId);
         fileDTO.setName(fileName);
         fileDTO.setLabDataUpload(createdFileId);
-        fileDTO.setType(FileType);
-        fileDTO.setFilePath(FilePath);
+        fileDTO.setType(fileType);
+        fileDTO.setFilePath(filePath);
+        // Create file
         fileService.create(fileDTO);
         return new ResponseEntity<>(createdFileId, HttpStatus.CREATED);
     }
-
+    /**
+     * Update an existing file.
+     */
     @PutMapping("/{id}")
     public ResponseEntity<Long> updateFile(
             @PathVariable(name = "id") final Long fileId,
             @RequestParam(name = "labRequestId") final Long labRequestId,
             @RequestPart("file") MultipartFile file) throws IOException {
-
         if (!fileService.exists(fileId)) {
             return ResponseEntity.notFound().build();
         }
-
-        // Update the file
+        // Extract file details
         String fileType = file.getContentType();
         String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-
         // Update lab data upload description
         LabDataUploadDTO labDataUploadDTO = new LabDataUploadDTO();
         labDataUploadDTO.setDescription(fileName);
         labDataUploadDTO.setLabRequest(labRequestId);
         labDataUploadService.update(fileService.get(fileId).getLabDataUpload(), labDataUploadDTO);
-
-        // Save the updated file
+        // Save the updated file to filesystem
         String filePath = fileService.saveFile(file);
-
-        // Update the file entity
+        // Update file entity
         FileDTO fileDTO = new FileDTO();
         fileDTO.setId(fileId);
         fileDTO.setName(fileName);
@@ -103,7 +106,9 @@ public class FileResource {
 
         return new ResponseEntity<>(fileId, HttpStatus.CREATED);
     }
-
+    /**
+     * Delete a file by ID.
+     */
     @DeleteMapping("/{id}")
     @ApiResponse(responseCode = "204")
     public ResponseEntity<FileDTO> deleteFile(@PathVariable(name = "id") final Long id) {
@@ -111,7 +116,6 @@ public class FileResource {
         if (fileDTO != null) {
             // Get the file path from the file DTO
             String filePath = fileDTO.getFilePath();
-
             // Delete the file from the file system
             try {
                 Path path = Paths.get(filePath);
@@ -123,7 +127,6 @@ public class FileResource {
                 // Return an appropriate response to the client
                 return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
             }
-
             // Once the file is deleted from the file system, delete it from the database
             fileService.delete(id);
             return ResponseEntity.ok(fileDTO);
