02b3ae88d77f4041e67e39c37963cde46bc53256
herathua <110981456+herathua@users.noreply.github.com>
2024-05-02 21:03:05 +0530

    add ipfs controller RequestMapping

--- src/main/java/io/bootify/health_hive/rest/IPFSController.java
+++ src/main/java/io/bootify/health_hive/rest/IPFSController.java
@@ -11,18 +11,19 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@RequestMapping(value = "/api/ipfs", produces = MediaType.APPLICATION_JSON_VALUE)
 public class IPFSController {
 
 
     @Autowired
     private IPFSService ipfsService;
 
-    @PostMapping(value = "upload")
+    @PostMapping(value = "/upload")
     public String uploadFile(@RequestParam("file") MultipartFile file) {
         return ipfsService.saveFile(file);
     }
 
-    @GetMapping(value = "file/{hash}")
+    @GetMapping(value = "/file/{hash}")
     public ResponseEntity<byte[]> getFile(@PathVariable("hash") String hash) {
         HttpHeaders headers = new HttpHeaders();
         headers.set("Content-type", MediaType.ALL_VALUE);
