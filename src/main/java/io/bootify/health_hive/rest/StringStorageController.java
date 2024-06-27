b6025b1b996a3111704feccc3493383d4333332b
Chamika Sandun <110515504+Dodamwaththacs@users.noreply.github.com>
2024-06-27 14:37:13 +0530

    add docker file for and modification for the first deployment.

--- src/main/java/io/bootify/health_hive/rest/StringStorageController.java
+++ src/main/java/io/bootify/health_hive/rest/StringStorageController.java
@@ -2,14 +2,12 @@ package io.bootify.health_hive.rest;
 
 import io.bootify.health_hive.service.StringStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
+
 import java.math.BigInteger;
 
 @RestController
+@RequestMapping("/api")
 public class StringStorageController {
 
     @Autowired
--- src/main/java/io/bootify/health_hive/service/StringStorageService.java
+++ src/main/java/io/bootify/health_hive/service/StringStorageService.java
@@ -17,14 +17,14 @@ public class StringStorageService {
     private final StringStorage stringStorage;
 
     public StringStorageService() {
-        Web3j web3j = Web3j.build(new HttpService("http://localhost:8545")); // URL of your Ethereum node
-        Credentials credentials = Credentials.create("0xae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f"); // Wallet private key
+        Web3j web3j = Web3j.build(new HttpService("http://ganache-hh:8545")); // URL of your Ethereum node
+        Credentials credentials = Credentials.create("0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3"); // Wallet private key
 
         BigInteger gasPrice = BigInteger.valueOf(20_000_000_000L); // Custom gas price
         BigInteger gasLimit = BigInteger.valueOf(4_300_000); // Custom gas limit
         ContractGasProvider gasProvider = new StaticGasProvider(gasPrice, gasLimit);
 
-        stringStorage = StringStorage.load("0x8CdaF0CD259887258Bc13a92C0a6dA92698644C0", web3j, credentials, gasProvider);
+        stringStorage = StringStorage.load("0x345cA3e014Aaf5dcA488057592ee47305D9B3e10", web3j, credentials, gasProvider);
     }
 
     public String storeString(String inputString) throws Exception {
