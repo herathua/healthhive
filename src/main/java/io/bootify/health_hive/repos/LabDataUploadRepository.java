d3e051c9f3a2ce5bcbe0f16409798152f96d4083
herathua <110981456+herathua@users.noreply.github.com>
2024-07-03 15:21:42 +0530

    add pinata cloud - put & get file is completed

--- src/main/java/io/bootify/health_hive/repos/LabDataUploadRepository.java
+++ src/main/java/io/bootify/health_hive/repos/LabDataUploadRepository.java
@@ -3,13 +3,14 @@ package io.bootify.health_hive.repos;
 import io.bootify.health_hive.domain.LabDataUpload;
 import io.bootify.health_hive.domain.LabRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import java.util.List;
-
 
 public interface LabDataUploadRepository extends JpaRepository<LabDataUpload, Long> {
 
     LabDataUpload findFirstByLabRequest(LabRequest labRequest);
 
-    List<LabDataUpload> findByLabRequestId(Long labRequestId);
+    @Query("SELECT l FROM LabDataUpload l WHERE l.labRequest.id = :labRequestId")
+    List<LabDataUpload> findByLabRequestId(@Param("labRequestId") Long labRequestId);
 }
