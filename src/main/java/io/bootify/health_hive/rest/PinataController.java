f685ce1e1570554fffb58cb4c8427b373e40a9d2
herathua <110981456+herathua@users.noreply.github.com>
2024-07-03 12:06:02 +0530

    add pinata cloud - get file is completed

--- src/main/java/io/bootify/health_hive/rest/PinataController.java
+++ src/main/java/io/bootify/health_hive/rest/PinataController.java
@@ -1,25 +1,31 @@
 package io.bootify.health_hive.rest;
 
 import io.bootify.health_hive.service.PinataService;
-import org.apache.hc.core5.http.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/api/pinata")
+@RequestMapping(value = "/api")
 public class PinataController {
 
     @Autowired
     private PinataService pinataService;
 
-    @PostMapping("/pin")
-    public String pinFile(@RequestParam String filePath) {
-        try {
-            return pinataService.pinFileToIPFS(filePath);
-        } catch (IOException | ParseException e) {
-            return "Error pinning file: " + e.getMessage();
-        }
+    @PostMapping(value = "/pinata/upload")
+    public String uploadFile(@RequestParam("file") MultipartFile file) {
+        return pinataService.saveFile(file);
+    }
+
+    @GetMapping(value = "/pinata/{hash}")
+    public ResponseEntity<byte[]> getFile(@PathVariable("hash") String hash) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-type", MediaType.ALL_VALUE);
+        byte[] bytes = pinataService.loadFile(hash);
+        return ResponseEntity.status(HttpStatus.OK).headers(headers).body(bytes);
     }
 }
